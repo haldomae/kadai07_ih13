f7d23bf589a941aa0ab20a22a22c0db0d0b98f6d
haldomae <hal.domae@gmail.com>
2024-11-07 17:21:14 +0900

    [バグ改修]SDKのバージョンを更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hal_domae.kadai07_ih13"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hal_domae.kadai07_ih13"
