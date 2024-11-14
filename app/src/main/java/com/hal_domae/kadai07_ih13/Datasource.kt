c42cf37d9c667ab70cdab6e6b11422f48359bc1e
haldomae <hal.domae@gmail.com>
2024-11-14 15:10:15 +0900

    図鑑データを作成

---
+++ app/src/main/java/com/hal_domae/kadai07_ih13/Datasource.kt
@@ -0,0 +1,12 @@
+package com.hal_domae.kadai07_ih13
+
+val listData = listOf(
+    mapOf("image" to R.drawable.img_1, "name" to "モンスター01", "explain" to "モンスター01の詳細"),
+    mapOf("image" to R.drawable.img_2, "name" to "モンスター02", "explain" to "モンスター02の詳細"),
+    mapOf("image" to R.drawable.img_3, "name" to "モンスター03", "explain" to "モンスター03の詳細"),
+    mapOf("image" to R.drawable.img_4, "name" to "モンスター04", "explain" to "モンスター04の詳細"),
+    mapOf("image" to R.drawable.img_5, "name" to "モンスター05", "explain" to "モンスター05の詳細"),
+    mapOf("image" to R.drawable.img_6, "name" to "モンスター06", "explain" to "モンスター06の詳細"),
+    mapOf("image" to R.drawable.img_7, "name" to "モンスター07", "explain" to "モンスター07の詳細"),
+    mapOf("image" to R.drawable.img_8, "name" to "モンスター08", "explain" to "モンスター08の詳細"),
+)
